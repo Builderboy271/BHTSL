ffe70c22aed80d18c0e44c481684d78abdebc471
Builderboy271 <Builderboy271@proton.me>
2026-03-01 04:05:11 -0600

    Update update.js
    Attempt to auto-update from BHTSL instead of HTSL

--- update/update.js
+++ update/update.js
@@ -22,20 +22,20 @@ function versionCompare(myVersion, minimumVersion) {
 let load = register("worldLoad", () => {
     try {
         axios({
-            url: "https://raw.githubusercontent.com/BusterBrown1218/HTSL/main/metadata.json",
+            url: "https://raw.githubusercontent.com/Builderboy271/BHTSL/main/metadata.json",
             method: 'GET'
         }).then(response => {
             const latestVersion = response.data.version;
-            const currentVersion = JSON.parse(FileLib.read("HTSL", "./metadata.json")).version;
+            const currentVersion = JSON.parse(FileLib.read("BHTSL", "./metadata.json")).version;
             if (versionCompare(currentVersion, latestVersion)) {
-                if (Settings.loadMessage) ChatLib.chat(`&3[HTSL] &fLoaded successfully!`);
+                if (Settings.loadMessage) ChatLib.chat(`&3[BHTSL] &fLoaded successfully!`);
                 return;
             }
-            ChatLib.chat(new Message(new TextComponent("&3[HTSL] &fNew HTSL version available!").setClick("open_url", "https://github.com/BusterBrown1218/HTSL/releases")));
+            ChatLib.chat(new Message(new TextComponent("&3[BHTSL] &fNew BHTSL version available!").setClick("open_url", "https://github.com/Builderboy271/BHTSL/releases")));
     
         });
     } catch (error) {
-        ChatLib.chat("&3[HTSL] &cError while checking version");
+        ChatLib.chat("&3[BHTSL] &cError while checking version");
     }
     load.unregister();
 });
@@ -45,7 +45,8 @@ function directoryExists(directoryPath) {
     return dir.exists() && dir.isDirectory();
 }
 
-if (!directoryExists("./config/ChatTriggers/modules/HTSL/imports")) {
-    FileLib.write("HTSL", "./imports/default.htsl", "playerWeather Raining\nplayerTime 1000\n\n// Does anyone even read this?\nteamstat test Blue set 12\nteamvar test Blue set 12 false\nglobalstat test set 12\nglobalvar test set 12 false\nstat test set 12\nvar test set 12 false", true);
-    FileLib.write("HTSL", "./imports/stone.json", "{\"item\": \"{id:\\\"minecraft:stone\\\",Count:1b,Damage:0s}\"}", true);
+if (!directoryExists("./config/ChatTriggers/modules/BHTSL/imports")) {
+    FileLib.write("BHTSL", "./imports/default.htsl", "playerWeather Raining\nplayerTime 1000\n\n// Does anyone even read this?\nteamstat test Blue set 12\nteamvar test Blue set 12 false\nglobalstat test set 12\nglobalvar test set 12 false\nstat test set 12\nvar test set 12 false", true);
+    FileLib.write("BHTSL", "./imports/stone.json", "{\"item\": \"{id:\\\"minecraft:stone\\\",Count:1b,Damage:0s}\"}", true);
+
 }
